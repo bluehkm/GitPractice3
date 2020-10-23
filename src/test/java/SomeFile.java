94ea64d81faee1c64393b848c18040fd7335e136
bluehkm <bluehakim@protonmail.com>
2020-10-23 20:37:11 +0300

    new word

--- src/test/java/SomeFile.java
+++ src/test/java/SomeFile.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class SomeFile {
     public static void main(String[] args) {
-        System.out.println("Hello Yavuz");
+        System.out.println("Hello hkm");
         System.out.println("New word");
+        System.out.println("another word");
 
     }
 }
